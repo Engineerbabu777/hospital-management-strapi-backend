53b45e41009b252a10292552f7d91188e6efcac0
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-18 17:05:03 +0500

    add:/some

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,3 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({});
+exports.default = () => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: process.env.CLOUDINARY_NAME,
+                api_key: process.env.CLOUDINARY_KEY,
+                api_secret: process.env.CLOUDINARY_SECRET,
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+});
