7a92265f99a2fc413d672233d61f166a5844c1ae
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-18 17:05:03 +0500

    add:/some

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
     'strapi::logger',
     'strapi::errors',
-    'strapi::security',
     'strapi::cors',
     'strapi::poweredBy',
     'strapi::query',
@@ -11,4 +10,24 @@ exports.default = [
     'strapi::session',
     'strapi::favicon',
     'strapi::public',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'connect-src': ["'self'", 'https:'],
+                    'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+                    'media-src': [
+                        "'self'",
+                        'data:',
+                        'blob:',
+                        'market-assets.strapi.io',
+                        'res.cloudinary.com',
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
+        },
+    },
 ];
